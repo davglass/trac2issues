e5b5cacbaf7fbc9b4f76a60300091d6e53b291c3
Dav Glass <davglass@gmail.com>
2009-04-20 08:36:21 -0700

    Import is now working and labels are being created. Still need comment API

--- trac2issues.py
+++ trac2issues.py
@@ -14,6 +14,12 @@ pp = pprint.PrettyPrinter(indent=4)
 parser = OptionParser()
 parser.add_option('-t', '--trac', dest='trac', help='Path to the Trac project to export.')
 parser.add_option('-p', '--project', dest='project', help='Name of the GitHub Project to import into.')
+parser.add_option('-x', '--closed', action="store_true", default=False, dest='closed', help='Include closed tickets.')
+parser.add_option('-c', '--component', action="store_true", default=False, dest='component', help='Create a label for the Trac component.')
+parser.add_option('-m', '--milestone', action="store_true", default=False, dest='milestone', help='Create a label for the Trac milestone.')
+parser.add_option('-o', '--owner', action="store_true", default=False, dest='owner', help='Create a label for the Trac owner.')
+parser.add_option('-r', '--reporter', action="store_true", default=False, dest='reporter', help='Add a comment naming the reporter.')
+parser.add_option('-u', '--url', dest='url', help='The base URL for the trac install (will also link to the old ticket in a comment).')
 
 (options, args) = parser.parse_args(sys.argv[1:])
 
@@ -25,7 +31,6 @@ class ImportTickets:
     def __init__(self, trac=options.trac, project=options.project):
         self.env = open_environment(trac)
         self.trac = trac
-        self.includeClosed = True
         self.project = project
         self.now = datetime.now(utc)
         #Convert the timestamp from a float to an int to drop the .0
@@ -36,9 +41,30 @@ class ImportTickets:
         except TracError, e:
             print_error(e.message)
 
+        self.includeClosed = options.closed
+        self.labelMilestone = options.milestone
+        self.labelComponent = options.component
+        self.labelOwner = options.owner
+        self.labelReporter = options.reporter
+        self.useURL = False
+
+        if options.url:
+            self.useURL = "%s/ticket/" % options.url
+
+        
         self.ghAuth()
         
         self.checkProject()
+
+        if self.useURL:
+            print bold('Does this look like a valid trac url? [y/N]\n %s1234567' % self.useURL)
+            go = sys.stdin.readline().strip().lower()
+
+            if go[0:1] != 'y':
+                print_error('Try Again..')
+            
+        ##We own this project..
+        self._fetchTickets()
 
     def checkProject(self):
         url = "%s/repos/show/%s/%s" % (self.github, self.login, self.project)
@@ -46,8 +72,6 @@ class ImportTickets:
         if 'error' in data:
             print_error("%s/%s: %s" % (self.login, self.project, data['error'][0]['error']))
         
-        ##We own this project..
-        self._fetchTickets()
 
     def ghAuth(self):
         login = os.popen('git config --global github.user').read().strip()
@@ -62,7 +86,6 @@ class ImportTickets:
         self.token = token
 
     def _fetchTickets(self):
-        print bold(red('_fetchTickets'))
         cursor = self.db.cursor()        
         
         where = " where (status != 'closed') "
@@ -78,10 +101,10 @@ class ImportTickets:
                 'id': id,
                 'summary': summary,
                 'description': description,
-                'milestone': milestone,
-                'component': component,
-                'reporter': reporter,
-                'owner': owner,
+                'milestone': milestone.replace(' ', '_'),
+                'component': component.replace(' ', '_'),
+                'reporter': reporter.replace(' ', '_'),
+                'owner': owner.replace(' ', '_'),
                 'history': []
             }
             cursor2 = self.db.cursor()        
@@ -97,18 +120,71 @@ class ImportTickets:
 
             tickets.append(ticket)
 
+        print bold('About to import (%s) tickets from Trac to %s/%s.\n%s? [y/N]' % (len(tickets), self.login, self.project, red('Are you sure you wish to continue')))
+        go = sys.stdin.readline().strip().lower()
+
+        if go[0:1] != 'y':
+            print_error('Import Aborted..')
+
+
         #pp.pprint(tickets)
         for data in tickets:
             self.createIssue(data)
 
         
-    def createIssue(self, data):
-        print bold('Creating issue')
-        pp.pprint(data)
-        #url = 'http://gist.github.com/gists'
-        #data = urllib.urlencode(out)
-        #req = urllib2.Request(url, data)
-        #response = urllib2.urlopen(req)
+    def createIssue(self, info):
+        print bold('Creating issue.')
+        out = {
+            'login': self.login,
+            'token': self.token,
+            'title': info['summary'],
+            'body': info['description']
+        }
+        data = urllib.urlencode(out)
+
+        url = "%s/issues/open/%s/%s" % (self.github, self.login, self.project)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
+        ticket_data = simplejson.load(response)
+
+        if 'number' in ticket_data['issue']:
+            num = ticket_data['issue']['number']
+            print bold('Issue #%s created.' % num)
+        else:
+            print_error('GitHub didn\'t return an issue number :(')
+
+        if self.labelMilestone and 'milestone' in info:
+            self.createLabel(num, "m:%s" % info['milestone'])
+
+        if self.labelComponent and 'component' in info:
+            self.createLabel(num, "c:%s" % info['component'])
+
+        if self.labelOwner and 'owner' in info:
+            self.createLabel(num, "o:%s" % info['owner'])
+
+        if self.labelReporter and 'reporter' in info:
+            self.createLabel(num, "r:%s" % info['reporter'])
+
+        if self.useURL:
+            comment = "Ticket imported from Trac:\n %s%s" % (self.useURL, info['id'])
+            self.addComment(num, comment)
+            
+
+    def createLabel(self, num, name):
+        print bold("\tAdding label %s to issue # %s" % (name, num))
+        url = "%s/issues/label/add/%s/%s/%s/%s" % (self.github, self.login, self.project, name, num)
+        out = {
+            'login': self.login,
+            'token': self.token
+        }
+        data = urllib.urlencode(out)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
+        label_data = simplejson.load(response)
+        
+    def addComment(self, num, comment):
+        print bold("\tAdding comment to issue # %s" % num)
+        print red(bold('\tNot Working yet, need API access.'))
         
 
 ##Format bold text
